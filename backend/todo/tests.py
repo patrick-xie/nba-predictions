889a3fc1d1d002f3575baddf29bcff04583df2ea
Patrick Xie <47607915+patrick-xie@users.noreply.github.com>
2021-02-15 18:11:12 -0500

    unit testing added for backend

--- backend/todo/tests.py
+++ backend/todo/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
 
 # Create your tests here.
+from todo.models import Todo
+
+class TodoTestCase(TestCase):
+    def setUp(self):
+        Todo.objects.create(title="rockets", description="suck")
+        Todo.objects.create(title="wizards", description="suck more")
+
+    def test_Todos_work(self):
+        rockets = Todo.objects.get(description="suck")
+        wizards = Todo.objects.get(description="suck more")
+        self.assertEqual(rockets._str_(), 'rockets')
+        self.assertEqual(wizards._str_(), 'wizards')
--- backend/todo/views.py
+++ backend/todo/views.py
@@ -3,7 +3,7 @@ from rest_framework import viewsets
 from .serializers import TodoSerializer
 from .models import Todo
 # Create your views here.
-
+# http://localhost:8000/api/todos/
 class TodoView(viewsets.ModelViewSet):
     serializer_class = TodoSerializer
     queryset = Todo.objects.all()
